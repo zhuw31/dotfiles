be1cc0492c1b7fe6bd1a1e13d76137b369b8a289
zhuw31 <meiyegg@gmail.com>
2021-12-20 18:34:36 +0800

    Add dot_config/nvim/lua/zhuw31/cmp.lua

---
+++ dot_config/nvim/lua/zhuw31/cmp.lua
@@ -0,0 +1,50 @@
+local cmp = require'cmp'
+local lspkind = require('lspkind')
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+      end,
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' },
+    }, {
+      { name = 'buffer' },
+    }),
+    formatting = {
+        format = lspkind.cmp_format({
+          with_text = false,
+          maxwidth = 50,
+          before = function (_, vim_item)
+            return vim_item
+          end
+        })
+      }
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
