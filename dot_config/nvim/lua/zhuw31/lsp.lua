08378a5d7aa05e1af5476f065ef048ff74b626ad
zhuw31 <meiyegg@gmail.com>
2021-12-21 23:55:14 +0800

    Update dot_config/nvim/lua/zhuw31/lsp.lua

--- dot_config/nvim/lua/zhuw31/lsp.lua
+++ dot_config/nvim/lua/zhuw31/lsp.lua
@@ -1,17 +1,19 @@
-local nvim_lsp = require('lspconfig')
-local lsp_installer = require("nvim-lsp-installer")
+local nvim_lsp = require 'lspconfig'
+local lsp_installer = require 'nvim-lsp-installer'
 
 local servers = {
-    'tsserver',
-    'cssls',
-    'cssmodules_ls',
-    'html',
-    'jsonls',
-    'eslint',
-    'stylelint_lsp',
-    'yamlls',
-    'vimls',
-    'sumneko_lua',
+  'tsserver',
+  'cssls',
+  'cssmodules_ls',
+  'html',
+  'jsonls',
+  'eslint',
+  'stylelint_lsp',
+  'yamlls',
+  'vimls',
+  'sumneko_lua',
+  'bashls',
+  'vuels',
 }
 
 -- auto install listed servers
@@ -19,17 +21,83 @@ for _, name in pairs(servers) do
   local server_is_found, server = lsp_installer.get_server(name)
   if server_is_found then
     if not server:is_installed() then
-      print("Installing " .. name)
+      print('Installing ' .. name)
       server:install()
     end
   end
 end
 
-local on_attach = function(_, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local opts = { noremap=true, silent=true }
+-- null-ls
+local null_ls = require 'null-ls'
+local prefer_local = { prefer_local = 'node_modules/.bin' }
+local sources = {
+  null_ls.builtins.formatting.prettier.with(prefer_local),
+  null_ls.builtins.formatting.stylua,
+  null_ls.builtins.diagnostics.eslint.with(prefer_local),
+  null_ls.builtins.diagnostics.stylelint.with(prefer_local),
+  null_ls.builtins.code_actions.eslint.with(prefer_local),
+}
 
-  -- our first remap
+null_ls.setup {
+  sources = sources,
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd 'autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()'
+    end
+  end,
+}
+
+-- signs
+local signs = {
+  { name = 'DiagnosticSignError', text = '' },
+  { name = 'DiagnosticSignWarn', text = '' },
+  { name = 'DiagnosticSignHint', text = '' },
+  { name = 'DiagnosticSignInfo', text = '' },
+}
+
+for _, sign in ipairs(signs) do
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+end
+
+local config = {
+  virtual_text = false,
+  signs = {
+    active = signs,
+  },
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
+}
+vim.diagnostic.config(config)
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = 'rounded',
+})
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = 'rounded',
+})
+
+local function lsp_highlight_document(client)
+  -- Set autocommands conditional on server_capabilities
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+      false
+    )
+  end
+end
+
+local opts = { noremap = true, silent = true }
+
+local function lsp_keymaps(bufnr)
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   -- buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
@@ -46,28 +114,63 @@ local on_attach = function(_, bufnr)
   -- buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   -- buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   -- buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  -- buf_set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+local on_attach = function(client, bufnr)
+  lsp_keymaps(bufnr)
+  lsp_highlight_document(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
+-- ts-util
+local ts_utils = require 'nvim-lsp-ts-utils'
+local function setup_tsserver()
+  local init_options = ts_utils.init_options
+  local ts_on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+    ts_utils.setup {
+      auto_inlay_hints = false,
+    }
+    ts_utils.setup_client(client)
+
+    on_attach(client, bufnr)
+  end
+
+  return {
+    init_options = init_options,
+    on_attach = ts_on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- install all servers
 lsp_installer.on_server_ready(function(server)
-  -- Specify the default options which we'll use to setup all servers
   local default_opts = {
     on_attach = on_attach,
     capabilities = capabilities,
   }
 
-  -- Now we'll create a server_opts table where we'll specify our custom LSP server configuration
   local server_opts = {
-    -- Provide settings that should only apply to the "eslintls" server
-    ["eslintls"] = function()
+    ['sumneko_lua'] = function()
       default_opts.settings = {
-        format = {
-          enable = true,
+        lua = {
+          diagnostics = {
+            globals = { 'vim' },
+          },
+          workspace = {
+            library = {
+              [vim.fn.expand '$VIMRUNTIME/lua'] = true,
+              [vim.fn.stdpath 'config' .. '/lua'] = true,
+            },
+          },
         },
       }
+    end,
+    ['tsserver'] = function()
+      default_opts = setup_tsserver()
     end,
   }
 
@@ -75,10 +178,3 @@ lsp_installer.on_server_ready(function(server)
   local server_options = server_opts[server.name] and server_opts[server.name]() or default_opts
   server:setup(server_options)
 end)
-
--- for _, server in ipairs(servers) do
--- 	nvim_lsp[server].setup {
---         on_attach = on_attach,
---         capabilities = capabilities,
---     }
--- end
