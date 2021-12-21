d651c13c8e5f291317c67f7442d9ea364b4cf5eb
zhuw31 <meiyegg@gmail.com>
2021-12-21 13:15:53 +0800

    Update dot_config/nvim/lua/zhuw31/gitsigns.lua

--- dot_config/nvim/lua/zhuw31/gitsigns.lua
+++ dot_config/nvim/lua/zhuw31/gitsigns.lua
@@ -1,5 +1,12 @@
 require'gitsigns'.setup {
     current_line_blame = true,
-    keymaps = {}
+    keymaps = {},
+    signs = {
+      add = { hl = 'GitSignsAdd', text = '▌' },
+      change = { hl = 'GitSignsChange', text = '▌' },
+      delete = { hl = 'GitSignsDelete', text = '▌' },
+      topdelete = { hl = 'GitSignsDelete', text = '▌' },
+      changedelete = { hl = 'GitSignsChange', text = '▌' },
+    },
 }
 
