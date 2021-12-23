87dc3ec8183d0af7e7012cad09b6a86756e00a22
zhuw31 <meiyegg@gmail.com>
2021-12-23 17:50:08 +0800

    Update dot_config/nvim/lua/zhuw31/lsp.lua

--- dot_config/nvim/lua/zhuw31/lsp.lua
+++ dot_config/nvim/lua/zhuw31/lsp.lua
@@ -23,17 +23,21 @@ end
 
 -- null-ls
 local null_ls = require 'null-ls'
-local null_ls_opts = { prefer_local = 'node_modules/.bin' }
 local sources = {
-  null_ls.builtins.formatting.prettier.with(null_ls_opts),
+  null_ls.builtins.formatting.prettierd,
   null_ls.builtins.formatting.stylua,
-  null_ls.builtins.diagnostics.eslint.with(null_ls_opts),
-  null_ls.builtins.diagnostics.stylelint.with(null_ls_opts),
-  null_ls.builtins.code_actions.eslint.with(null_ls_opts),
+  null_ls.builtins.diagnostics.eslint_d,
+  null_ls.builtins.diagnostics.stylelint.with { prefer_local = 'node_modules/.bin' },
+  null_ls.builtins.code_actions.eslint_d,
 }
 
 null_ls.setup {
   sources = sources,
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd 'autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()'
+    end
+  end,
 }
 
 -- signs
