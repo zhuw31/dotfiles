501c28e597558f50dda79170a0d0f751e53e15bf
zhuw31 <meiyegg@gmail.com>
2021-12-23 10:16:54 +0800

    Update dot_config/nvim/lua/zhuw31/bufferline.lua Update dot_config/nvim/lua/zhuw31/lsp.lua

--- dot_config/nvim/lua/zhuw31/bufferline.lua
+++ dot_config/nvim/lua/zhuw31/bufferline.lua
@@ -1,3 +1,11 @@
+local opts = { silent = true, noremap = true }
+local function n_map(key, cmd)
+  return vim.api.nvim_set_keymap('n', key, cmd, opts)
+end
+
+n_map('[b', '<cmd>BufferLineCyclePrev<CR>')
+n_map(']b', '<cmd>BufferLineCycleNext<CR>')
+
 require('bufferline').setup {
   options = {
     show_close_icon = true,
