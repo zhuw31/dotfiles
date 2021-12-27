c7ec9700eb338268915a1ca8630d0b473932b723
zhuw31 <meiyegg@gmail.com>
2021-12-27 23:44:56 +0800

    Update dot_config/nvim/lua/zhuw31/options.lua

--- dot_config/nvim/lua/zhuw31/options.lua
+++ dot_config/nvim/lua/zhuw31/options.lua
@@ -34,6 +34,14 @@ end
 -- Set colorscheme
 cmd [[colorscheme tokyonight]]
 
+-- Highlight when yank
+cmd [[
+    augroup highlight_yank
+        autocmd!
+        autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 40})
+    augroup END
+]]
+
 -- options
 local options = {
   hidden = false,
