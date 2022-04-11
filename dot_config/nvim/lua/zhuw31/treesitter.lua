30cc6523c14fc9658a1cbfd3327ee09a2710a880
zhuw31 <meiyegg@gmail.com>
2022-04-11 14:19:27 +0800

    Update dot_config/nvim/lua/zhuw31/treesitter.lua

--- dot_config/nvim/lua/zhuw31/treesitter.lua
+++ dot_config/nvim/lua/zhuw31/treesitter.lua
@@ -1,14 +1,29 @@
 local configs = require 'nvim-treesitter.configs'
 
 configs.setup {
-  ensure_installed = 'maintained',
+  ensure_installed = {
+    'bash',
+    'css',
+    'dockerfile',
+    'fish',
+    'go',
+    'graphql',
+    'html',
+    'javascript',
+    'jsonc',
+    'lua',
+    'python',
+    'rust',
+    'toml',
+    'tsx',
+    'typescript',
+    'vue',
+    'yaml',
+  },
   highlight = {
     enable = true,
   },
   matchup = {
-    enable = true,
-  },
-  indent = {
     enable = true,
   },
   context_commentstring = {
