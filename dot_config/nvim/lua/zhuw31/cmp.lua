7dc4d3ed6344f33351ccc6f44ff779055880f5bd
zhuw31 <meiyegg@gmail.com>
2022-04-18 00:00:58 +0800

    Update dot_config/nvim/lua/zhuw31/cmp.lua

--- dot_config/nvim/lua/zhuw31/cmp.lua
+++ dot_config/nvim/lua/zhuw31/cmp.lua
@@ -13,6 +13,16 @@ vim.g.vsnip_filetypes = {
   javascriptreact = { 'javascript' },
   typescriptreact = { 'javascript', 'typescript' },
 }
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 vim.cmd [[
 imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
 smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
@@ -27,29 +37,31 @@ cmp.setup {
     end,
   },
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable,
-    ['<C-e>'] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    },
-    ['<Tab>'] = cmp.mapping.confirm { select = true },
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.confirm({ select = true })
+        else
+            fallback()
+        end
+    end, {
+      'i',
+      's',
+    }),
   },
   sources = cmp.config.sources {
     { name = 'nvim_lsp' },
     { name = 'path' },
     { name = 'vsnip' },
-    { name = 'buffer', keyword_length = 5 },
+    { name = 'buffer', keyword_length = 5, max_item_count = 5 },
   },
   formatting = {
     format = lspkind.cmp_format {
-      with_text = true,
-      menu = {
-        buffer = '[BUF]',
-        nvim_lsp = '[LSP]',
-        path = '[PATH]',
-        vsnip = '[SNIP]',
-      },
+      mode = 'symbol_text',
       maxwidth = 50,
       before = function(_, vim_item)
         return vim_item
