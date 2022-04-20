5d8edf7d6500b53724551dfddac1ac8cdda45d1b
zhuw31 <meiyegg@gmail.com>
2022-04-20 10:00:24 +0800

    Update dot_config/nvim/lua/zhuw31/bufferline.lua

--- dot_config/nvim/lua/zhuw31/bufferline.lua
+++ dot_config/nvim/lua/zhuw31/bufferline.lua
@@ -10,12 +10,10 @@ utils.n_map('<S-l>', '<cmd>BufferLineCycleNext<CR>')
 
 bufferline.setup {
   options = {
-    mode = 'tab',
     show_close_icon = true,
     show_buffer_close_icons = false,
     diagnostics = 'nvim_lsp',
     always_show_bufferline = true,
-    separator_style = 'thick',
     offsets = {
       {
         filetype = 'NvimTree',
