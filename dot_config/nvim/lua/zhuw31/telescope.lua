a8357a7fdd2318dc889a4fa57f73d66bff642469
zhuw31 <meiyegg@gmail.com>
2022-05-07 14:14:24 +0800

    Update dot_config/nvim/lua/zhuw31/telescope.lua

--- dot_config/nvim/lua/zhuw31/telescope.lua
+++ dot_config/nvim/lua/zhuw31/telescope.lua
@@ -3,11 +3,6 @@ if not status_ok then
   return
 end
 
-local actions_status_ok, actions = pcall(require, 'telescope.actions')
-if not actions_status_ok then
-  return
-end
-
 local opts = { silent = true }
 vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', opts)
 vim.keymap.set('n', '<leader>fl', ':Telescope live_grep<CR>', opts)
@@ -16,14 +11,6 @@ vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', opts)
 vim.keymap.set('n', '<leader>fk', ':Telescope keymaps<CR>', opts)
 
 telescope.setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous,
-      },
-    },
-  },
   pickers = {
     find_files = {
       theme = 'dropdown',
