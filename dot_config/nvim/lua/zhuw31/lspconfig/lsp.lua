f0afc97338b0f748ff0e1f48bc1c752db3ae9b31
zhuw31 <meiyegg@gmail.com>
2022-05-07 12:54:29 +0800

    Update dot_config/nvim/lua/zhuw31/lspconfig/lsp.lua

--- dot_config/nvim/lua/zhuw31/lspconfig/lsp.lua
+++ dot_config/nvim/lua/zhuw31/lspconfig/lsp.lua
@@ -66,14 +66,6 @@ local config = {
     border = 'rounded',
     header = '',
     prefix = '',
-    format = function(d)
-      local t = vim.deepcopy(d)
-      local code = d.code or d.user_data.lsp.code
-      if code then
-        t.message = string.format('%s [%s]', t.message, code):gsub('1. ', '')
-      end
-      return t.message
-    end,
   },
 }
 
