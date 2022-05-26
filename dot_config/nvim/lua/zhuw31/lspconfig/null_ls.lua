93fe5b715d7edbdd0e312bbf75dd10f783be8a87
zhuw31 <meiyegg@gmail.com>
2022-05-26 11:00:53 +0800

    Update dot_config/nvim/lua/zhuw31/lspconfig/null_ls.lua

--- dot_config/nvim/lua/zhuw31/lspconfig/null_ls.lua
+++ dot_config/nvim/lua/zhuw31/lspconfig/null_ls.lua
@@ -11,20 +11,6 @@ local sources = {
   null_ls.builtins.code_actions.eslint_d,
 }
 
-local lsp_formatting = vim.api.nvim_create_augroup('LspFormatting', {})
-local on_attach = function(client, bufnr)
-  if client.supports_method 'textDocument/formatting' then
-    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      group = lsp_formatting,
-      buffer = bufnr,
-      -- on 0.8, you should use vim.lsp.buf.format instead
-      callback = vim.lsp.buf.formatting_sync,
-    })
-  end
-end
-
 null_ls.setup {
   sources = sources,
-  on_attach = on_attach,
 }
