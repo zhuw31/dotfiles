0ad963e9960293b654aaac831c7138d0e0d3e8be
zhuw31 <meiyegg@gmail.com>
2022-07-30 14:06:14 +0800

    feat(nvim): remove bind for `cmp.mapping.complete()`

--- dot_config/nvim/lua/zhuw31/cmp.lua
+++ dot_config/nvim/lua/zhuw31/cmp.lua
@@ -52,7 +52,6 @@ function M.setup()
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<C-space>'] = cmp.mapping.complete(),
       ['<C-y>'] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
         select = true,
