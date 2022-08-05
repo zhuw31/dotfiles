98fb160014d4b9d0ead9a4d47d94dcf99846a7ad
zhuw31 <meiyegg@gmail.com>
2022-08-05 23:21:47 +0800

    feat(nvim): use floating window to display updating results

--- dot_config/nvim/lua/zhuw31/plugins.lua
+++ dot_config/nvim/lua/zhuw31/plugins.lua
@@ -25,55 +25,64 @@ vim.cmd [[
 
 local packer = require 'packer'
 
-packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'dstein64/vim-startuptime'
-  use 'shaunsingh/nord.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    'nvim-telescope/telescope-fzy-native.nvim',
-    'nvim-lua/popup.nvim',
-    'nvim-lua/plenary.nvim',
-  }
-  use {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig',
-    'jose-elias-alvarez/typescript.nvim',
-    'jose-elias-alvarez/null-ls.nvim',
-    'folke/lua-dev.nvim',
-    'b0o/schemastore.nvim',
-  }
-  use {
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'saadparwaiz1/cmp_luasnip',
-    'L3MON4D3/LuaSnip',
-  }
-  use 'kyazdani42/nvim-web-devicons'
-  use 'hoob3rt/lualine.nvim'
-  use 'gpanders/editorconfig.nvim'
-  use {
-    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
-    'JoosepAlviste/nvim-ts-context-commentstring',
-  }
-  use 'windwp/nvim-autopairs'
-  use 'numToStr/Comment.nvim'
-  use 'tpope/vim-surround'
-  use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
-  use 'mzlogin/vim-markdown-toc'
-  use 'kyazdani42/nvim-tree.lua'
-  use 'lewis6991/gitsigns.nvim'
-  use 'akinsho/toggleterm.nvim'
+packer.startup {
+  function(use)
+    use 'wbthomason/packer.nvim'
+    use 'dstein64/vim-startuptime'
+    use 'shaunsingh/nord.nvim'
+    use {
+      'nvim-telescope/telescope.nvim',
+      'nvim-telescope/telescope-fzy-native.nvim',
+      'nvim-lua/popup.nvim',
+      'nvim-lua/plenary.nvim',
+    }
+    use {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
+      'jose-elias-alvarez/typescript.nvim',
+      'jose-elias-alvarez/null-ls.nvim',
+      'folke/lua-dev.nvim',
+      'b0o/schemastore.nvim',
+    }
+    use {
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
+    }
+    use 'kyazdani42/nvim-web-devicons'
+    use 'hoob3rt/lualine.nvim'
+    use 'gpanders/editorconfig.nvim'
+    use {
+      { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    }
+    use 'windwp/nvim-autopairs'
+    use 'numToStr/Comment.nvim'
+    use 'tpope/vim-surround'
+    use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
+    use 'mzlogin/vim-markdown-toc'
+    use 'kyazdani42/nvim-tree.lua'
+    use 'lewis6991/gitsigns.nvim'
+    use 'akinsho/toggleterm.nvim'
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    packer.sync()
-  end
-end)
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      packer.sync()
+    end
+  end,
+  config = {
+    display = {
+      open_fn = function()
+        return require('packer.util').float { border = 'single' }
+      end,
+    },
+  },
+}
