8405ffdbc104137705106711e5402fd4aefbe6ca
zhuw31 <meiyegg@gmail.com>
2022-08-13 11:17:46 +0800

    chore(nvim): Comment.nvim breaking change

--- dot_config/nvim/lua/zhuw31/comment.lua
+++ dot_config/nvim/lua/zhuw31/comment.lua
@@ -11,14 +11,14 @@ local comment_utils = require 'Comment.utils'
 comment.setup {
   pre_hook = function(ctx)
     local location = nil
-    if ctx.ctype == comment_utils.ctype.block then
+    if ctx.ctype == comment_utils.ctype.blockwise then
       location = ts_utils.get_cursor_location()
     elseif ctx.cmotion == comment_utils.cmotion.v or ctx.cmotion == comment_utils.cmotion.V then
       location = ts_utils.get_visual_start_location()
     end
 
     return ts_internals.calculate_commentstring {
-      key = ctx.ctype == comment_utils.ctype.line and '__default' or '__multiline',
+      key = ctx.ctype == comment_utils.ctype.linewise and '__default' or '__multiline',
       location = location,
     }
   end,
