584f840d7fa0f1f1d977b32eb91b5880ae82f292
zhuw31 <meiyegg@gmail.com>
2022-08-20 09:54:13 +0800

    feat(nvim): update ts_context_commentstring

--- dot_config/nvim/lua/zhuw31/comment.lua
+++ dot_config/nvim/lua/zhuw31/comment.lua
@@ -4,22 +4,8 @@ if not ok or not ok_ts then
   return
 end
 
-local ts_utils = require 'ts_context_commentstring.utils'
-local ts_internals = require 'ts_context_commentstring.internal'
-local comment_utils = require 'Comment.utils'
+local comment_nvim = require 'ts_context_commentstring.integrations.comment_nvim'
 
 comment.setup {
-  pre_hook = function(ctx)
-    local location = nil
-    if ctx.ctype == comment_utils.ctype.blockwise then
-      location = ts_utils.get_cursor_location()
-    elseif ctx.cmotion == comment_utils.cmotion.v or ctx.cmotion == comment_utils.cmotion.V then
-      location = ts_utils.get_visual_start_location()
-    end
-
-    return ts_internals.calculate_commentstring {
-      key = ctx.ctype == comment_utils.ctype.linewise and '__default' or '__multiline',
-      location = location,
-    }
-  end,
+  pre_hook = comment_nvim.create_pre_hook(),
 }
