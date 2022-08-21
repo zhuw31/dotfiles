f959274dd9b6bbc9d805c8f006691f895a0ae583
zhuw31 <meiyegg@gmail.com>
2022-08-21 22:53:28 +0800

    feat(wezterm): init wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wz = require 'wezterm'
+
+wz.on('update-right-status', function(window)
+  local date = wz.strftime '%a %m-%d %H:%M'
+
+  window:set_right_status(wz.format {
+    { Text = wz.nerdfonts.mdi_clock .. ' ' .. date },
+  })
+end)
+
+return {
+  font_size = 16.0,
+  font = wz.font 'sf mono',
+  color_scheme = 'nord',
+
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+}
