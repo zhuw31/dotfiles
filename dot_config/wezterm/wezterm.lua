03936b12bde01f2541ed5999dfada2ab81871cff
zhuw31 <meiyegg@gmail.com>
2022-08-22 12:01:25 +0800

    feat(wezterm): use default font and window_padding

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -10,15 +10,7 @@ end)
 
 return {
   font_size = 16.0,
-  font = wz.font 'sf mono',
   color_scheme = 'nord',
-
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
 
   keys = {
     { key = 'd', mods = 'CMD', action = wz.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
