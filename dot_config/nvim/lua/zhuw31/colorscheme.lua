3a4a70228dac57f8efda01bd53cb4611845b587e
zhuw31 <meiyegg@gmail.com>
2022-12-05 09:49:35 +0800

    feat(nvim): check colorscheme available

--- dot_config/nvim/lua/zhuw31/colorscheme.lua
+++ dot_config/nvim/lua/zhuw31/colorscheme.lua
@@ -1,10 +1,11 @@
--- Set colorscheme
-local colorscheme = 'catppuccin'
-require(colorscheme).setup {
+local ok, colorscheme = pcall(require, 'catppuccin')
+
+colorscheme.setup {
   flavour = 'frappe',
   background = {
     light = 'latte',
     dark = 'frappe',
   },
 }
+
 vim.cmd.colorscheme(colorscheme)
