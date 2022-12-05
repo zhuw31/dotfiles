5272d102c0fdc25494f54a6134a22501ca635351
zhuw31 <meiyegg@gmail.com>
2022-12-05 23:19:26 +0800

    Revert "feat(nvim): use <a-*> in luasnip"
    This reverts commit f3b251602e58b61b03b1912cf3afdcbc6913b5ac.

--- dot_config/nvim/lua/zhuw31/luasnip.lua
+++ dot_config/nvim/lua/zhuw31/luasnip.lua
@@ -130,19 +130,19 @@ ls.add_snippets('typescript', {
   ),
 })
 
-vim.keymap.set({ 'i', 's' }, '<a-k>', function()
+vim.keymap.set({ 'i', 's' }, '<c-k>', function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
 end)
 
-vim.keymap.set({ 'i', 's' }, '<a-j>', function()
+vim.keymap.set({ 'i', 's' }, '<c-j>', function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
 end)
 
-vim.keymap.set('i', '<a-l>', function()
+vim.keymap.set('i', '<c-l>', function()
   if ls.choice_active() then
     ls.change_choice(1)
   end
