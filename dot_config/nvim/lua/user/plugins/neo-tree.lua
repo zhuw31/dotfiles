a56b72f351b0d9205488e8f6828eb924fdf94bc0
zhuw31 <meiyegg@gmail.com>
2023-01-09 11:17:23 +0800

    feat(nvim): reindent neo-tree config

--- dot_config/nvim/lua/user/plugins/neo-tree.lua
+++ dot_config/nvim/lua/user/plugins/neo-tree.lua
@@ -1,23 +1,23 @@
 return {
   {
-	  "nvim-neo-tree/neo-tree.nvim",
-	  cmd = "Neotree",
-	  dependencies = { 
-		  "nvim-lua/plenary.nvim",
-		  "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		  "MunifTanjim/nui.nvim",
-	  },
-	  keys = {
-		  { "<leader>ft", "<cmd>Neotree toggle<CR>", desc = "Toggle [F]ile [T]ree" },
-	  },
-	  opts = {
-		  filesystem = {
-			  visible = true,
-			  follow_current_file = true,
-		  },
-	  },
-	  init = function()
-		  vim.g.neo_tree_remove_legacy_commands = 1
-	  end,
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle [F]ile [T]ree" },
+    },
+    opts = {
+      filesystem = {
+        visible = true,
+        follow_current_file = true,
+      },
+    },
+    init = function()
+      vim.g.neo_tree_remove_legacy_commands = 1
+    end,
   }
 }
