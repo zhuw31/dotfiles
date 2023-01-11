31e95b450d1ac9e1c5b7e756a2e27b04dd667041
zhuw31 <meiyegg@gmail.com>
2023-01-11 22:32:29 +0800

    chore(nvim): remove nvim configs

--- dot_config/nvim/lua/user/plugins/core.lua
+++
@@ -1,9 +0,0 @@
-local specs = {
-  -- set to HEAD for now. I'm sill making too many changes for this repo related to lazy itself
-  -- { "folke/lazy.nvim", version = false },
-  { "folke/lazy.nvim" },
-}
-
-require("user.configs").setup()
-
-return specs
