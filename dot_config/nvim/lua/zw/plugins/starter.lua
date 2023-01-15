c12f7b15f198108b4c8f72f050c6683c328dc52c
zhuw31 <meiyegg@gmail.com>
2023-01-15 09:55:12 +0800

    feat(nvim): config min.starter

--- dot_config/nvim/lua/zw/plugins/starter.lua
+++ dot_config/nvim/lua/zw/plugins/starter.lua
@@ -28,9 +28,7 @@ return {
           new_section("Find file",    "Telescope find_files", "Telescope"),
           new_section("Recent files", "Telescope oldfiles",   "Telescope"),
           new_section("Grep text",    "Telescope live_grep",  "Telescope"),
-          new_section("init.lua",     "e $MYVIMRC",           "Config"),
           new_section("Lazy",         "Lazy",                 "Config"),
-          new_section("New file",     "ene | startinsert",    "Built-in"),
           new_section("Quit",         "qa",                   "Built-in"),
         },
         content_hooks = {
