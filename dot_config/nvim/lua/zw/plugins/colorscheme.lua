4b66bd0383d4852f194a1320421337699c7f9d4c
zhuw31 <meiyegg@gmail.com>
2023-01-28 11:03:07 +0800

    feat(nvim): add catppuccin integrations

--- dot_config/nvim/lua/zw/plugins/colorscheme.lua
+++ dot_config/nvim/lua/zw/plugins/colorscheme.lua
@@ -4,8 +4,16 @@ return {
     name = "catppuccin",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("catppuccin").load("frappe")
+    opts = {
+      flavour = "frappe",
+      integrations = {
+        neotree = true,
+      },
+    },
+    config = function(_, opts)
+      local catppuccin = require("catppuccin")
+      catppuccin.setup(opts)
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
