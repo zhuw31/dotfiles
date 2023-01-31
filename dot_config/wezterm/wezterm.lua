7ea6ceb060dbce93de4bfbb720e1eafa0b7dc5e8
zhuw31 <meiyegg@gmail.com>
2023-01-31 11:00:16 +0800

    feat(wezterm): try wezterm again

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+return {
+  font_size = 16.0,
+  color_scheme = 'Catppuccin Frappe',
+  use_fancy_tab_bar = false,
+  font_antialias = 'None',
+  line_height = 1.2,
+  window_padding = {
+    bottom = 0,
+  },
+}
