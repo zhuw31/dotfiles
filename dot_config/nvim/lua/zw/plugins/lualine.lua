ced9f4da24b61f6cd85db713f5bee6f106d87a58
zhuw31 <meiyegg@gmail.com>
2023-02-02 11:47:35 +0800

    feat(nvim): remove noice

--- dot_config/nvim/lua/zw/plugins/lualine.lua
+++ dot_config/nvim/lua/zw/plugins/lualine.lua
@@ -39,26 +39,6 @@ return {
             { "filename", path = 0 },
           },
           lualine_x = {
-            -- stylua: ignore
-            {
-              function()
-                return require("noice").api.status.command.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.command.has()
-              end,
-              color = fg("Statement"),
-            },
-            -- stylua: ignore
-            {
-              function()
-                return require("noice").api.status.mode.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.mode.has()
-              end,
-              color = fg("Constant"),
-            },
             {
               require("lazy.status").updates,
               cond = require("lazy.status").has_updates,
