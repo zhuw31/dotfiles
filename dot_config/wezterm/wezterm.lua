c6dec3271b37162c66a5e5cabb3a9970816b92f7
zhuw31 <meiyegg@gmail.com>
2023-02-11 11:05:28 +0800

    feat(wezterm): update config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,16 @@
+local wezterm = require("wezterm")
+
 return {
   font_size = 16.0,
-  color_scheme = 'Catppuccin Frappe',
+  font = wezterm.font_with_fallback({
+    { family = "SauceCodePro Nerd Font", weight = "Regular" },
+    { family = "SauceCodePro Nerd Font", weight = "Bold" },
+  }),
+  color_scheme = "Catppuccin Frappe",
   use_fancy_tab_bar = false,
-  font_antialias = 'None',
   line_height = 1.2,
   window_padding = {
     bottom = 0,
   },
+  window_decorations = "NONE",
 }
