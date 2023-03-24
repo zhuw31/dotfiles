5cd550f71a9b6dbd3d8546aa1e43bb245653038b
zhuw31 <meiyegg@gmail.com>
2023-03-24 17:40:22 +0800

    "Commented out font fallback options in wezterm.lua for better font rendering."

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,10 +2,10 @@ local wezterm = require("wezterm")
 
 return {
   font_size = 16.0,
-  font = wezterm.font_with_fallback({
-    { family = "SauceCodePro Nerd Font", weight = "Regular" },
-    { family = "SauceCodePro Nerd Font", weight = "Bold" },
-  }),
+  -- font = wezterm.font_with_fallback({
+  --   { family = "SauceCodePro Nerd Font", weight = "Regular" },
+  --   { family = "SauceCodePro Nerd Font", weight = "Bold" },
+  -- }),
   color_scheme = "nord",
   enable_tab_bar = false,
   line_height = 1.2,
