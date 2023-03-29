62cefc475d9ccd6eda8c38eeb313e6aea48ed0dc
zhuw31 <meiyegg@gmail.com>
2023-03-29 15:46:46 +0800

    Refactor Wezterm configuration to use config_builder and set font to SauceCodePro Nerd Font in Git commit message

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,16 +1,24 @@
 local wezterm = require("wezterm")
 
-return {
-  font_size = 16.0,
-  -- font = wezterm.font_with_fallback({
-  --   { family = "SauceCodePro Nerd Font", weight = "Regular" },
-  --   { family = "SauceCodePro Nerd Font", weight = "Bold" },
-  -- }),
-  color_scheme = "nord",
-  enable_tab_bar = false,
-  line_height = 1.2,
-  window_padding = {
-    bottom = 0,
-  },
-  window_decorations = "RESIZE",
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'nord'
+config.font_size = 16.0
+config.font = wezterm.font 'SauceCodePro Nerd Font'
+config.enable_tab_bar = false
+config.line_height = 1.2
+config.window_padding = {
+  right = 0,
+  bottom = 0,
+  left = 0,
+  top = 0,
 }
+config.window_decorations = "RESIZE"
+
+return config
