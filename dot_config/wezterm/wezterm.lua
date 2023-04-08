0d1d271454de3c8449c4b11ff5eeadd3db0a9f44
zhuw31 <meiyegg@gmail.com>
2023-04-08 19:43:15 +0800

    Change wezterm colorscheme to nord

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = "Oceanic-Next"
+config.color_scheme = "nord"
 config.font_size = 16.0
 config.font = wezterm.font("SauceCodePro Nerd Font")
 config.enable_tab_bar = false
