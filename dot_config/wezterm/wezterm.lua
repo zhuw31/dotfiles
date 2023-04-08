bd158150654599c8befc2fa05edca96b6731f169
zhuw31 <meiyegg@gmail.com>
2023-04-08 19:40:21 +0800

    Change wezterm theme to Oceanic-Next

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,15 +2,13 @@ local wezterm = require("wezterm")
 
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'nord'
+config.color_scheme = "Oceanic-Next"
 config.font_size = 16.0
-config.font = wezterm.font 'SauceCodePro Nerd Font'
+config.font = wezterm.font("SauceCodePro Nerd Font")
 config.enable_tab_bar = false
 config.line_height = 1.2
 config.window_padding = {
