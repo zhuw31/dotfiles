408e630012096bfc8c529a9c88abfaec21027b37
zhuw31 <meiyegg@gmail.com>
2023-04-26 16:05:13 +0800

    Adjust zsh configs

---
+++ dot_config/zsh/exports.zsh
@@ -0,0 +1,21 @@
+# Misc
+export LANG="en_US.UTF-8"
+export EDITOR=nvim
+export HOMEBREW_BUNDLE_FILE="~/.Brewfile"
+export TEALDEER_CONFIG_DIR="~/.config/tealdeer"
+
+# pnpm
+export PNPM_HOME="/Users/zhuwei/Library/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
+# bun completions
+[ -s "/Users/zhuwei/.bun/_bun" ] && source "/Users/zhuwei/.bun/_bun"
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Golang
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+export PATH=$PATH:/usr/local/go/bin
